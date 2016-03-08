2d8d53adbb53f5532dca322d90840ef310c6e9bd
Tyler Boire <tylerboire@gmail.com>
2016-03-07 20:39:13 -0500

    changed to fit standards

--- parse.py
+++ parse.py
@@ -10,33 +10,28 @@ parser.add_option("-o", "--output", action="store", dest="outDir", help="Where w
 
 (options, args) = parser.parse_args()
 '''
+
 testOutDir = "/home/tyler/testing"
 testMalware = '/home/tyler/Downloads/malware'
 
-def apkUnpack(malware, outDir):
-    run = 'apktool d ' + malware + " -o " + outDir
+def apk_unpack(malware, outDir):
+    run = 'apktool d ' + malware + ' -o ' + outDir
     os.system(run)
-    cert(testOutDir)
-
-
+    
 def cert(outDir):
-    list = os.listdir(outDir + "/original/META-INF/")
-    for files in list:
-        if files.endswith('.RSA'):
-            run = "keytool --printcert -file " + outDir + "/original/META-INF/" + files
+    meta_files = os.listdir(outDir + '/original/META-INF/')
+    for meta_file in meta_files:
+        if meta_file.endswith('.RSA'):
+            run = 'keytool --printcert -file ' + outDir + '/original/META-INF/' + files
             os.system(run)
-    dex2jar(testMalware,testOutDir)
-
 
 def dex2jar(malware, outOut):
     run = 'unzip ' + malware + ' -d /tmp/malwareTmp'
     os.system(run)
-    run = "d2j-dex2jar.sh /tmp/malwareTmp/classes.dex -o " + outOut + "/SourceCode.jar"
+    run = 'd2j-dex2jar.sh /tmp/malwareTmp/classes.dex -o ' + outOut + '/SourceCode.jar'
     os.system(run)
 
-    nicePermissions(testOutDir)
-
-def nicePermissions(outDir):
+def nice_permissions(outDir):
 
     manifest = outDir + '/AndroidManifest.xml'
 
@@ -47,4 +42,8 @@ def nicePermissions(outDir):
     for i in range(0, len(permissions)):
         print(permissions[i].attrib['{http://schemas.android.com/apk/res/android}name'])
 
-apkUnpack(testMalware,testOutDir)
+if __name__ = '__main__':
+	apk_unpack(testMalware,testOutDir)
+	cert(testOutDir)
+	dex2jar(testMalware, testOutDir)
+	nice_permissions(testOutDir)
